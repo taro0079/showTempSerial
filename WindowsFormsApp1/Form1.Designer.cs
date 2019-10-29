46757e6af785a0bade30555f5ba764e87c509d7f
太郎 森田 <keith_1105@outlook.jp>
2019-10-29 09:44:06 +0900

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(33, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(453, 32);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "BaudRate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(33, 184);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(197, 32);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Data Read";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 444);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 61);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "data";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(208, 444);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 61);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(541, 47);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.MarkerColor = System.Drawing.Color.Blue;
+            series2.Name = "temp";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(782, 912);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1399, 1031);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // stopボタンを無効化
+            button4.Enabled = false;
+
+            // レートに9600を追加
+            comboBox2.Items.Add(9600);
+
+            // デフォルトで選択されるレートを決定
+            if (comboBox2.Items.Count > 0)
+            {
+                comboBox2.SelectedIndex = 0;
+            }
+
+            // 接続されているSerialぽーとを選択
+            string[] ports = SerialPort.GetPortNames();
+
+            // 接続されているポートを全てコンボボックスに追加
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+            
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                MessageBox.Show("シリアルポートは既に接続されています", "シリアルポート");
+            }
+            else
+            {
+                serialPort1.BaudRate = Convert.ToInt32(comboBox2.SelectedItem);
+                serialPort1.Parity = Parity.None;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.PortName = Convert.ToString(comboBox1.SelectedItem);
+                serialPort1.Open();
+                button1.Enabled = false;
+            }
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (serialPort1.IsOpen)
+            {
+                button2.Enabled = false;
+                button4.Enabled = true;
+
+                // 測定開始時刻を取得
+                DateTime startTime = DateTime.Now;
+
+
+                serialPort1.Write("*RST" + "\n"); // Reset device setting (device command)
+                serialPort1.Write(":SENS:FUNC 'TEMP'" + "\n"); // set to temperature mode (device command)
+                serialPort1.Write(":SENS:CHAN 1" + "\n");
+                serialPort1.Write(":SENS:TEMP:TRAN TC" + "\n");
+                serialPort1.Write(":SENS:TEMP:RJUN:RSEL INTERNAL" + "\n");
+                serialPort1.Write(":SENS:TEMP:RJUN:SIM 0" + "\n");
+                serialPort1.Write(":SENS:TEMP:TC K" + "\n");
+
+                
+                for (; ; ) // 無限ループ
+                {
+                    
+
+                    // 各データ点取得時刻を所得
+                    DateTime nowTime = DateTime.Now;
+                    
+
+                    // データ取得時刻 - 測定開始時刻
+                    TimeSpan dt = nowTime - startTime;
+                    double Dt = Convert.ToDouble(dt.TotalMinutes); // double に変換
+                    double DT = Math.Round(Dt, 3); //四捨五入
+                    serialPort1.Write(":READ?" + "\n");
+                    string str = serialPort1.ReadLine();
+                    label4.Text = str;
+                    label5.Text = Convert.ToString(DT);
+                    double data = Convert.ToDouble(str);
+
+                    
+
+                    
+                    // chartへの書き込み
+                    chart1.Series["temp"].Points.AddXY(DT, data);
+
+                    // csv ファイルへの書き込み
+                    
+                    string filename = startTime.ToString("yyyy-MM-dd-hh-mm-ss")+ "data.csv";
+                    StreamWriter file = new StreamWriter(filename, true, Encoding.UTF8);
+                    file.WriteLine(nowTime + "," + data + ",");
+                    file.Close();
+
+                    
+
+                    await Task.Run(() =>
+                    {
+                        System.Threading.Thread.Sleep(2000);
+                    }
+                    );
+
+                    if (button4.Enabled == false)
+                    {
+                        button2.Enabled = true;
+                        break;
+                    }
+
+                }
+                
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
